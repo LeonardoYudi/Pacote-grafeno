1ac19a4de06e1bfa8d3c9664a605976a3ce41f22
Your Name <leonardo.iwama25@outlook.com>
2023-08-01 09:22:25 -0400

    feat: all

---
+++ src/components/TailButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+defineProps<{ text: string }>()
+
+</script>
+
+<template>
+  <button class="w-10 h-5 border rounded-md bg-stone-700 text-white">{{ text }}</button>
+</template>
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import BasicButton from "./components/BasicButton.vue";
+import TailButton from "./components/TailButton.vue"
+export {BasicButton ,TailButton};
